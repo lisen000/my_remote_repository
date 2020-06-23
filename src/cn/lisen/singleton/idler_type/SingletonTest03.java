a3aae756dc80aea5ed29b5c7c82ff477ecc9858d
my_local_repository <1920198566@qq.com>
2020-06-23 20:00:45 +0800

    完善注释

--- src/cn/lisen/singleton/idler_type/SingletonTest03.java
+++ src/cn/lisen/singleton/idler_type/SingletonTest03.java
@@ -15,7 +15,7 @@ class Singleton{
 	private static Singleton instance;
 	private Singleton() {}
 	//提供一个静态的共有方法，当使用到该方法时，才去instance
-	//即懒汉式
+	//即懒汉式，并发下不安全
 	public static Singleton getInstance() {
 		if(instance==null) {
 			instance = new Singleton();
